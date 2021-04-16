95fdcd8ae08ffc6003a84052a3e882b289f5df6c
AbdelhamidBassa <thebiggyburger@gmail.com>
2021-04-16 17:34:33 +0200

    Initial commit

---
+++ app/src/main/java/p17/android/nivigationactivity/ui/map/MapViewModel.java
@@ -0,0 +1,19 @@
+package p17.android.nivigationactivity.ui.map;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MapViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
