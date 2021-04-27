089e416fa05e33afc6d1c3bb3057cb1993fc43e6
AbdelhamidBassa <thebiggyburger@gmail.com>
2021-04-27 15:20:43 +0200

    add fragment_info

--- app/src/main/java/p17/android/nivigationactivity/MainActivity.java
+++ app/src/main/java/p17/android/nivigationactivity/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_map, R.id.nav_list)
+                R.id.nav_home, R.id.nav_map, R.id.nav_list, R.id.nav_info)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
